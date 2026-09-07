7ed3eba57ef833462a586d09d3fa8aa1a8e01a67
arush-07 <pradhanarush73@gmail.com>
2026-09-08 02:16:17 +0530

    Organize M6 platform under purva_m6

--- contracts/__init__.py
+++ contracts/__init__.py
@@ -1,4 +1,2 @@
+# PURVA | contracts/__init__.py | shared repository contracts
 CONTRACT_VERSION = "1.0.0"
-# PURVA | contracts/__init__.py | owner: M6
-# shared contracts package
-# TODO: implement. See docs/ for the module spec.
